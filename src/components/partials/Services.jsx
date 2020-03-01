c33c2acc8874693ee14d756c53fecaa0b3c93f70
Naeem Ahmed <bdnaeem3@gmail.com>
2020-03-01 07:18:21 +0600

    Flipping Card Icon and slider text fixed

--- src/components/partials/Services.jsx
+++ src/components/partials/Services.jsx
@@ -7,6 +7,7 @@ import FlippingCard, {CardFront, CardBack} from '../cards/FlippingCard'
 
 // Images
 import Cover from '../../assets/img/slider/banner3.jpg'
+import logo from '../../logo.svg';
 
 export default () => {
     return(
@@ -22,12 +23,7 @@ export default () => {
                     <FlippingCard>
                         <CardFront
                             cover={Cover}
-                            icon={<svg aria-hidden="true" focusable="false" data-prefix="fab" data-icon="facebook-f"
-                                       className="svg-inline--fa fa-facebook-f fa-w-10" role="img"
-                                       xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
-                                <path fill="currentColor"
-                                      d="M279.14 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.43 0 225.36 0c-73.22 0-121.08 44.38-121.08 124.72v70.62H22.89V288h81.39v224h100.17V288z"></path>
-                            </svg>}
+                            image={logo}
                             iconBG={`black`}
                             title={`After School`}
                         />
